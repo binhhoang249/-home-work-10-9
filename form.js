64bf9d598a3861948b6efd5e4c2cee8e8f8ba338
binhhoang249 <hoangthanhbinh24092005@gmail.com>
2024-09-12 00:49:56 +0700

    file2

--- form.js
+++ form.js
@@ -1,22 +1,20 @@
-var id = 0;
-function sell() {
-    id++;
+var No = 0;
+function add() {
+    No++;
     const form = document.querySelector('form[name="frm"]'); // lấy phần tử theo tên
     const pname = form.querySelector('#pname').value; 
     const qty = parseInt(form.querySelector('#quantity').value, 10);
     const price = parseFloat(form.querySelector('#price').value); 
-    const dc = parseFloat(form.querySelector('#dc').value);
-    if (!pname || isNaN(qty) || isNaN(price) || isNaN(dc)) {
+    if (!pname || isNaN(qty) || isNaN(price)) {
         alert('Vui lòng nhập đầy đủ thông tin và kiểm tra lại các giá trị của bạn');
         return;
     }
-    const sub = qty * price * (1 - dc / 100);
+    const sub = qty * price;
     var row = "<tr>";
-    row += "<td>" + id + "</td>";
+    row += "<td>" + No + "</td>";
     row += "<td>" + pname + "</td>";
     row += "<td>" + qty + "</td>";
     row += "<td>" + price.toFixed(2) + "</td>"; 
-    row += "<td>" + dc + "%</td>";
     row += "<td>" + sub.toFixed(2) + "</td>";
     row += "</tr>"; // kết thúc hàng
     document.getElementById("tbl").innerHTML += row;
